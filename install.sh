ffeb8338de44adb6045ea921b07e690c4a47de6d
Ross A. Baker <ross@rossabaker.com>
2015-11-30 10:55:05 -0500

    s/oh-my-zsh/prezto/

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-DOTDIR=$(dirname $(readlink -f $0))
-
-ln -sv $DOTDIR/git/gitconfig ~/.gitconfig
-ln -sv $DOTDIR/zsh/zshrc ~/.zshrc
-ln -sv $DOTDIR/zsh/ross.zsh-theme ~/.oh-my-zsh/custom

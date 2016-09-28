712658fd54e494b2cb88e5bbaed714369de9c95d
Ross A. Baker <ross@rossabaker.com>
2016-09-27 22:35:48 -0400

    Configure package

---
+++ .emacs.d/init.el
@@ -0,0 +1,21 @@
+;; Copyright (C) 2016 Ross A. Baker
+;; 
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3 of the License, or (at your option) any later
+;; version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+      '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)

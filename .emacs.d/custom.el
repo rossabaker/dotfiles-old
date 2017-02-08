a40dfcd001094fd82da645d334df1e906d8be11d
Ross A. Baker <ross@rossabaker.com>
2017-02-08 10:33:38 -0500

    Noise

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" default)))
+    ("9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" default)))
  '(package-selected-packages
    (quote
     (unfill edit-server web-mode hcl-mode markdown-mode undo-tree lorem-ipsum counsel swiper ivy zenburn-theme ensime ws-butler magit use-package))))

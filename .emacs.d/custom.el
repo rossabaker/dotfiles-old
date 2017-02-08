1fb9e097db30ae56367f44e58c5236ffa6166ab2
Ross A. Baker <ross@rossabaker.com>
2017-02-08 11:30:42 -0500

    Something is eating my ensime-sbt-command

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,6 +6,7 @@
  '(custom-safe-themes
    (quote
     ("9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" default)))
+ '(ensime-sbt-command nil)
  '(package-selected-packages
    (quote
     (exec-path-from-shell material-theme unfill edit-server web-mode hcl-mode markdown-mode undo-tree lorem-ipsum counsel swiper ivy zenburn-theme ensime ws-butler magit use-package))))

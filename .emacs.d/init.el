947791dba83b51cc6b1a82bfce2a0c1cae0bb87c
Ross A. Baker <ross@rossabaker.com>
2017-05-03 16:20:48 -0400

    Security first

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,3 +9,15 @@
 
 ;; This is the exciting part. This is like The Supremes.
 (org-babel-load-file (concat user-emacs-directory "settings.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((org-src-preserve-indentation . t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

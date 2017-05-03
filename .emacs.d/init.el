c1925f890a570e1a5a5cf0d91b0c698dd4536a20
Ross A. Baker <ross@rossabaker.com>
2017-05-03 16:08:29 -0400

    Bootstrap loading settings.org

---
+++ .emacs.d/init.el
@@ -0,0 +1,11 @@
+;; And I know the love I have for you
+;; Will grow and grow and grow, I think
+;; And so my love, I offer you
+;; A love that is strong, a prune that is true
+(require 'package)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+;; This is the exciting part. This is like The Supremes.
+(org-babel-load-file (concat user-emacs-directory "settings.org"))

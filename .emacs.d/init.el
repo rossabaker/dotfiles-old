695ef167e1cab0eb177b40a52a8a01ed71962265
Ross A. Baker <ross@rossabaker.com>
2017-05-13 01:20:06 -0400

    Hallelujah, Ensime connected

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,8 @@
 ;; A love that is strong, a prune that is true
 (require 'package)
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.org/packages/")))
+			 ("melpa" . "https://melpa.org/packages/")
+      			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
 (package-initialize)
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
--- .sbt/0.13/global.sbt
+++ .sbt/0.13/global.sbt
@@ -1,4 +1,2 @@
 cancelable in Global := true
 
-import org.ensime.EnsimeCoursierKeys._
-ensimeServerVersion in ThisBuild := "2.0.0-SNAPSHOT"

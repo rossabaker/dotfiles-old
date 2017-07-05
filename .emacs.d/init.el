8d34812d7845e1bdbeaee63eb29945f292aead8e
Ross A. Baker <ross@rossabaker.com>
2017-07-05 00:19:44 -0400

    Banish customizations to own file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,8 @@
 (package-initialize)
 
+;; Banish customizations to their ownfile
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file 'noerror)
+
 (require 'org)
 (org-babel-load-file (concat user-emacs-directory "config.org"))

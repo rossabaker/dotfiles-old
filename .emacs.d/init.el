ee83f170c5f9b6d45c2a52a5fc71063916cd3853
Ross A. Baker <ross@rossabaker.com>
2017-08-20 13:41:37 -0700

    Tabula rasa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,33 @@
+;; Packages
+;; https://glyph.twistedmatrix.com/2015/11/editor-malware.html
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+(require 'tls)
+(require 'gnutls)
+(let ((trustfile
+       (replace-regexp-in-string
+        "\\\\" "/"
+        (replace-regexp-in-string
+         "\n" ""
+         (shell-command-to-string "python -m certifi")))))
+  (setq tls-program
+        (list
+         (format "gnutls-cli%s --x509cafile %s -p %%p %%h"
+                 (if (eq window-system 'w32) ".exe" "") trustfile)))
+  (setq tls-checktrust t)
+  (setq gnutls-verify-error t)
+  (setq gnutls-trustfiles (list trustfile)))
 (package-initialize)
 
-;; Banish customizations to their ownfile
-(setq custom-file (concat user-emacs-directory "custom.el"))
-(load custom-file 'noerror)
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
 
-(require 'org)
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+;; Banish customizations to another file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)

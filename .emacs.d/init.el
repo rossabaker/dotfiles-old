ef20455cbe7a0ae6110ec38f3802ca47c65c9814
Ross A. Baker <ross@rossabaker.com>
2017-08-20 18:01:27 -0700

    Ivy and Counsel

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,9 +35,32 @@
 (use-package better-defaults
   :ensure t)
 
+(use-package counsel
+  :ensure t
+  :bind
+  (("M-x"     . counsel-M-x)
+   ("C-x C-f" . counsel-find-file)
+   ("<f1> f"  . counsel-describe-function)
+   ("<f1> v"  . counsel-describe-variable)
+   ("<f1> l"  . counsel-find-library)
+   ("<f2> i"  . counsel-info-lookup-symbol)
+   ("<f2> u"  . counsel-unicode-char)
+   ("C-c g"   . counsel-git)
+   ("C-c j"   . counsel-git-grep)
+   ("C-c k"   . counsel-ag)
+   ("C-x l"   . counsel-locate)))
+
 (use-package frame
   :config
   (blink-cursor-mode -1)
   (set-frame-font "Fira Code" nil t))
 
-
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :init
+  (setq ivy-use-virtual-buffers t)
+  :config
+  (ivy-mode 1)
+  :bind
+  (("C-c C-r" . ivy-resume)))

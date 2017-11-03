f97ca9371b8ffe4d1cff4907f10707701881967f
Ross A. Baker <ross@rossabaker.com>
2017-11-03 00:01:32 -0400

    use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,3 +19,12 @@
   (setq gnutls-trustfiles (list trustfile)))
 (package-initialize)
 
+;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
